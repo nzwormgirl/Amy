b136a5d0a6ea462f0e20111603778754465642f6
nzwormgirl <amylouisewhitehead@gmail.com>
2018-07-03 15:17:50 +1200

    added .repath

---
+++ R/repath.R
@@ -0,0 +1,12 @@
+
+# adapted from https://www.r-bloggers.com/stop-fiddling-around-with-copied-paths-in-windows-r/
+
+.repath <- function() {
+  cat("Paste windows file path and hit RETURN twice")
+  x <- scan(what = "")
+  xa <- gsub("\\\\", "/", x)
+  writeClipboard(paste(xa, collapse=" "))
+  cat("Here's your de-windowsified path. (It's also on the clipboard.)\n", xa, "\n")
+  
+}
+
