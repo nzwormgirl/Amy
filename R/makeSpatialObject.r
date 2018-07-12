e36492ce05dfe433bacb6ec0f7a735917871878a
nzwormgirl <amylouisewhitehead@gmail.com>
2018-07-12 16:17:24 +1200

    added addnewCoordinates function to create new columns of projected coordinates

--- R/makeSpatialObject.r
+++ R/makeSpatialObject.r
@@ -15,6 +15,7 @@
 makeSpatialObject <- function(myData,myX,myY,currentProj=NULL,newProj = NULL){
   require(sp)
   coordinates(myData) <- cbind(myData[,myX] , myData[,myY])
+
   if(!is.null(currentProj)) {
     proj4string(myData) <- CRS(currentProj)
     if(!is.null(newProj)){
@@ -24,3 +25,43 @@ makeSpatialObject <- function(myData,myX,myY,currentProj=NULL,newProj = NULL){
 
   return(myData)
 }
+
+#' addNewCoordinates
+#'
+#' This function allows you to add new columns of coordinates in a different projection.
+#' @param myData A data.frame to be made spatial - must include columns of spatial data
+#' @param myX Quoted name of column of easting or longitude coordinates
+#' @param myY Quoted name of cColumn of northing or latitude coordinates
+#' @param myNewX Quoted name of new column of easting or longitude coordinates
+#' @param myNewY Quoted name of new column of northing or latitude coordinates
+#' @param currentProj The current projection of the coordinate data as a character vector
+#' @param newProj A new projection as a character vector.
+#' @keywords shapefile
+#' @export
+#' @examples
+#' quakesSpatial <- addNewCoordinates(quakes,myX="long",myY="lat",myNewX = "easting",myNewY = "northing", currentProj = projWGS84, newProj = projNZTM)
+#'
+
+addNewCoordinates <- function(myData,myX,myY,myNewX = "X",myNewY = "Y",currentProj=NULL,newProj = NULL){
+
+  myNewX <- quo_name(myNewX)
+  myNewY <- quo_name(myNewY)
+
+  # identify rows with missing coordinates
+  myNArows <- which(rowSums(is.na(myData[,c(myX,myY)]))>0)
+
+  myOut <- bind_rows(
+    # calculate new coordinates for all rows with spatial data
+    as.data.frame(
+    makeSpatialObject(myData[-myNArows,],myX,myY,currentProj,newProj)
+  ) %>% dplyr::rename(!!myNewX := coords.x1,
+                      !!myNewY := coords.x2),
+  # add back in rows with missing coordinate data
+  myData[myNArows,]
+  )
+
+  return(myOut)
+}
+
+
+
